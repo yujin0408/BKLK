223e6b5843675a10c35f6e99c55c09236cc968be
yujin <aszx5849@naver.com>
2026-07-06 15:36:11 +0900

    chore: supabase 변수명 변경

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,5 +2,5 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.SUPABASE_ANON_KEY!,
 );
